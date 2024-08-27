ee0c17e1729bddd63d837b7e824bf402cfad01ca
KUMA439898 <KUMA439898@exactsoftware.com>
2024-08-27 12:15:57 +0530

    contract script is checked in.

---
+++ tests/contracts.spec.js
@@ -0,0 +1,44 @@
+
+const { test, expect } = require('@playwright/test');
+const data = require('../environment.json');
+const Login = require('../pages/loginPage');
+const newContract = require('../pages/createNewContractPage.js');
+const { waitForPaceLoader } = require('../utils/webUtils');
+
+
+test('it can create a new contract template from a blank template', async ({ page }) => {
+  const login = new Login(page);
+  const contract= new newContract(page);
+
+  const contractType="custom_contract";
+  const contractName="New blank contract";
+  const contractDescription="Hello World";
+
+  await page.goto(data.baseUrl + 'settings/contracts/templates/new');
+  await login.signIn(data.userName, data.password);
+  await waitForPaceLoader(page);
+  await contract.selectContractTypeAndFillContractName(contractType,contractName);
+  await contract.selectBlankTemplate();
+  await contract.fillBlankContractTemplate(contractDescription);
+  await contract.clickOnSaveBtn();  
+  await contract.verifyContractInTable(contractName);
+});
+
+test('it can create a new contract template from a word document', async ({ page }) => {
+  const contractType="custom_contract";
+  const contractName="New Work Contract";
+  const contractDescription="Hello World";
+  const filePath="fixtures/testTextdoc.docx";
+  
+  const login = new Login(page);
+  const contract= new newContract(page);
+
+  await page.goto(data.baseUrl + 'settings/contracts/templates/new');
+  await login.signIn(data.userName, data.password);
+  await waitForPaceLoader(page);
+  await contract.selectContractTypeAndFillContractName(contractType,contractName);
+  await contract.uploadWordDocument(filePath);
+  await contract.fillBlankContractTemplate(contractDescription);
+  await contract.clickOnSaveBtn();  
+  await contract.verifyContractInTable(contractName);
+});
--- utils/webUtils.js
+++ utils/webUtils.js
@@ -18,3 +18,50 @@ const waitForPaceLoader = async (page) => {
 
 // Exporting the function so it can be used in other files
 module.exports = { waitForPaceLoader };
+
+/**
+ * Wait until an element is removed or hidden
+ * @param {Page} page playwright page object
+ * @param {String} selector selector to wait for
+ */
+const waitForElementToDisappear = async (page, selector, tries = 'FIRST') => {
+  const INITIAL_TRIES = 20; // 20 * 500ms = 10 seconds
+  const TIMEOUT_BETWEEN_TRIES = 500;
+  const count = await page.locator(selector).count()
+  const isHidden = await page.locator(selector).isHidden()
+
+  // Only one element can be wait for
+  if (count > 1) {
+    throw new Error(`More than one element found for selector ${selector}`)
+  }
+
+  // One element should be visisble to wait for
+  if (count === 0 && tries === 'FIRST') {
+    throw new Error(`No element found for selector ${selector} to wait for`)
+  }
+
+  // If element is gone or hidden => resolve
+  if (count === 0 || isHidden) {
+    return true
+  }
+
+  // Set initial tries on first attempt
+  if (tries === 'FIRST') {
+    tries = INITIAL_TRIES
+  }
+
+  // Reduce tries by 1
+  tries = tries - 1
+
+  // If we are at the last try, throw an error
+  if (tries === 0) {
+    throw new Error(`Element ${selector} did not disappear`)
+  }
+
+  // Wait 10ms
+  await new Promise(resolve => setTimeout(resolve, TIMEOUT_BETWEEN_TRIES))
+
+  // Try again
+  return waitForElementToDisappear(page, selector, tries)
+}
+module.exports.waitForElementToDisappear = waitForElementToDisappear
