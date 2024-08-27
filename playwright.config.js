ecc29f8f1153cdeba9580b65f3f5211d467fd3f0
pvrajeshroyal86 <PASU440259@exactsoftware.com>
2024-08-27 13:00:19 +0530

    Config changes

--- playwright.config.js
+++ playwright.config.js
@@ -8,8 +8,7 @@ const config = {
         browserName: 'chromium',
         headless: false,
         launchOptions: { slowMo: 50 },
-        screenshot: 'only-on-failure',
-        viewport: { width: 1920, height: 1080 } // Adjust these values to your screen resolution
+        screenshot: 'only-on-failure'
     },
     workers: 1, // By default, all the tests are run in parallel, but we can limit the parallel run by keeping workers as 1
     reporter: 'allure-playwright',
